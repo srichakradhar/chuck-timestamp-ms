9ed2f02156888b3fb20555075a6c824e63814d68
Srichakradhar Reddy Nagireddy <srichakra3nsr3@gmail.com>
2016-11-17 13:21:36 +0000

    Timestamp Server - First Commit

--- server.js
+++ server.js
@@ -1,10 +1,25 @@
-var express = require('express')
-var app = express()
+var express = require('express');
+var app = express();
 
-app.get('/', function (req, res) {
-  res.send('Hello World!')
-})
+app.get('/:DATE_STRING', function (req, res) {
+  var date;
+  if(isNaN(parseInt(req.params.DATE_STRING, 10))){
+    date = new Date(req.params.DATE_STRING);
+  }else{
+    date = new Date(parseInt(req.params.DATE_STRING));
+  }
+  
+  var dateArray, naturalDate;
+  
+  if(Date.parse(date)){
+    dateArray = date.toDateString().split(' ');
+    naturalDate = dateArray[1] + ' ' + dateArray[2] + ', ' + dateArray[3];
+  }else{
+    naturalDate = null;
+  }
+  res.send({unix: Date.parse(date), natural: naturalDate});
+});
 
 app.listen(8080, function () {
-  console.log('Example app listening on port 8080!')
-})
+  console.log('Timestamp server listening on port 8080!');
+});
